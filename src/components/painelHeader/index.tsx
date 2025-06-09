0b79cf3b13a684ebaed58fcc87316ceb5c3db2e1
Talisson Dias <126925961+Talisson06@users.noreply.github.com>
2025-06-08 21:10:02 -0300

    Alterção Mobile

--- src/components/painelHeader/index.tsx
+++ src/components/painelHeader/index.tsx
@@ -9,8 +9,8 @@ export function DashboardHeader(){
     }
     
     return(
-        <div className=" w-full h-11 items-center flex bg-teal-600 rounded-lg text-white text-3xl font-karantina gap-20 px-4 mb-4
-                        md:text-lg">
+        <div className=" w-full h-11 items-center flex bg-teal-600 rounded-lg text-white text-lg font-karantina gap-20 px-4 mb-4
+                        md:text-2xl">
             <Link to="/dashboard">Dashboard</Link>
             <Link to="/dashboard/new">Cadastrar Novo Pet</Link>
 
